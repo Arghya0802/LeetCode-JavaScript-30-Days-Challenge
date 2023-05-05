5e591a06a2c23605af8b083358df7f9dfa6475a4
Arghya Chatterjee <96080808+Arghya0802@users.noreply.github.com>
2023-05-05 19:47:07 +0530

    Create Create Hello World Function.js

---
+++ Day 1/Create Hello World Function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        
+    return "Hello World" ;
+    
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+// Time Complexity:  O(1)
+// Space Complexity: O(1)
